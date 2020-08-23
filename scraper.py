e5c5871498afffec01b8fdbccad400f92518c9ea
Yaasir Mahamadou-Bachirou <yaasir.mahamadou@gmail.com>
2020-08-23 03:41:29 -0400

    scraper.py now gets all courses and credits for NCSU CS course and writes them to a text file.

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 website = requests.get('https://oucc.dasa.ncsu.edu/coe-14cscbs-nosubplan-2177/').text
 
@@ -9,7 +10,21 @@ article = soup.find('article')
 
 entry_content = article.find('div', class_='entry-content')
 
-#years = entry_content.h3.text
-for table in entry_content.find_all('table'):
-    classes = table.tbody
-    print(classes.text)
+#csv_file = open('ncsu_courses_scrape.csv','w')
+#csv_writer =csv.writer(csv_file)
+#csv_writer.writerow(['classes'])
+
+file=  open("ncsu_courses_scrape.txt", 'w')
+  
+for table in entry_content.find_all('table', class_='table table-striped'):
+    table_row = table.tbody.find_all('tr')
+    for row in table_row:
+        for x in row:
+            try:
+                #print(x.text)
+                file.write(row.text)
+            except:
+                pass
+        
+file.close()
+    
